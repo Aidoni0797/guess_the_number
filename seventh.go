d2db328bd81db21a4593f01e4fe5d74f553590cc
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-04-03 00:49:02 +0600

    seventh

---
+++ seventh.go
@@ -0,0 +1,16 @@
+func foo() {
+	// паникуем
+	panic("unexpected!")
+}
+
+func main() {
+	// выполняется при завершении main
+	defer func() {
+		// вызываем recover и сравниваем результат с nil
+		if r := recover(); r != nil {
+			fmt.Println(r) // выведет "unexpected"
+		}
+	}()
+	foo() // внутри foo срабатывает паника
+	fmt.Println("Вы не увидите это сообщение, так как в foo возникла паника")
+}
